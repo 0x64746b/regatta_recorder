e487706f81beb8d190bcc3ed5670e5cff6353ef8
dtk <dtk@gmx.de>
2016-09-25 17:26:29 +0200

    Translate `regrec` URLs

--- regrec/urls.py
+++ regrec/urls.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 
 from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.list_events, name='events'),
-    url(r'^events/(?P<event_id>\d+)/$', views.show_results_overview, name='results_overview'),
-    url(r'^races/(?P<race_id>\d+)/$', views.show_results_race, name='results_race'),
+    url(_(r'^events/(?P<event_id>\d+)/$'), views.show_results_overview, name='results_overview'),
+    url(_(r'^races/(?P<race_id>\d+)/$'), views.show_results_race, name='results_race'),
 ]

d25b9bf97c6560d91902ffc75482b100558900c6
dtk <dtk@gmx.de>
2016-09-25 21:12:28 +0200

    Make `Yacht` and `Run` admins inline
    Try to provide more wizard'ish guidance.

--- regrec/admin.py
+++ regrec/admin.py
@@ -2,7 +2,26 @@ from django.contrib import admin
 
 from .models import Event, Race, Run, Yacht
 
-admin.site.register(Event)
-admin.site.register(Race)
-admin.site.register(Run)
-admin.site.register(Yacht)
+
+class YachtInline(admin.TabularInline):
+    model = Yacht
+
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        YachtInline,
+    ]
+
+
+class RunInline(admin.TabularInline):
+    model = Run
+
+
+class RaceAdmin(admin.ModelAdmin):
+    inlines = [
+        RunInline,
+    ]
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Race, RaceAdmin)
